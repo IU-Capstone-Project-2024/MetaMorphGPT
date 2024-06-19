ec8b3e6402282ddc753290d0936914c7fdcf6744
Egopoler <egpol2004@yandex.ru>
2024-06-19 18:21:34 +0300

    Соединил код алмаза со своей llm. теперь по токену posp можно общаться с базовой моделью. Так же для каждого пользователя отдельная история, но пока poor code

--- src/test_llm_dialog.py
+++ src/test_llm_dialog.py
@@ -10,7 +10,6 @@ model = AutoModelForCausalLM.from_pretrained(model_name_1)
 # Функция для генерации ответа от модели
 def generate_response(prompt):
     inputs = tokenizer.encode(prompt, return_tensors="pt")
-    #outputs = model.generate(inputs, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id, repetition_penalty=1.3)
     outputs = model.generate(
     inputs,
     top_k=10,
@@ -29,27 +28,41 @@ def generate_response(prompt):
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return response
 
-# Основной цикл для общения с моделью
-print("Начните общение с моделью (введите 'exit' для выхода)")
 
-print(tokenizer.bos_token)
-history = "@@ПЕРВЫЙ@@ "
-while True:
-    user_input = input("Вы: ")
-    if user_input.lower() == "exit":
-        break
-    history += str(user_input + " @@ВТОРОЙ@@")
-    response = generate_response(history)
-    response_starts = len(history)
+
+
+def get_answer(history, user_input, model=model, tokenizer=tokenizer):
+
+    if history == "":
+        history = "@@ПЕРВЫЙ@@ "
     
-    #print("Модель: " + response) 
-    # раскомментить только для теста
-    
+    new_history = history + str(user_input + " @@ВТОРОЙ@@")
+
+    response_starts = len(new_history)
+
+    response = generate_response(new_history)
     
     only_response = response[response_starts:].replace("@@ВТОРОЙ@@", "").replace("@@ПЕРВЫЙ@@", "")
-    print("Обрезанная часть: " + only_response)
-
-    # Добавляем ответ в историю
-
-    history += " " + only_response + " @@ПЕРВЫЙ@@ "
     
+    new_history = " " + only_response + " @@ПЕРВЫЙ@@ "
+
+    return only_response, new_history
+
+
+def start_local_chat(model=model, tokenizer=tokenizer):
+    # Основной цикл для общения с моделью
+    print("Начните общение с моделью (введите 'exit' для выхода)")
+
+    print(tokenizer.bos_token)
+    history = ""
+    while True:
+        user_input = input("Вы: ")
+        if user_input.lower() == "exit":
+            break
+        
+        answer, history = get_answer(history, user_input)
+
+        print("Модель: " + answer)
+
+
+#start_local_chat()
